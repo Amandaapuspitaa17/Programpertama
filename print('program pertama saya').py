9f6cf223bddee4608212e899e8d71538372b83be
Amandaapuspitaa17 <147121175+Amandaapuspitaa17@users.noreply.github.com>
2023-10-05 20:56:21 -0700

    Add files via upload

---
+++ print('program pertama saya').py
@@ -0,0 +1,6 @@
+print('program pertama saya')
+print('saya Amanda Puspita')
+print('alamat saya Jelutung 2')
+print('hobi saya bermain bulu tangkis')
+print('saya berasal dari sekolah SMK 1 Payung')
+print(' saya lahir tanggal 17 Agustus 2004')
